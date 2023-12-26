418a913d07d85dee8ac4f9ed233b63e33df6b43f
Matheus Ferreira <ferreiromagrelo@gmail.com>
2023-12-26 12:55:50 -0300

    Fix n feat: random number after reset, press enter to press buttons

--- Challenges/jogo-do-adivinha/index.js
+++ Challenges/jogo-do-adivinha/index.js
@@ -1,6 +1,7 @@
+//variáveis
+
 let xAttempts = 0;
 let randomNumber = Math.round(Math.random() * 10);
-
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 const btnTry = document.querySelector("#btnTry");
@@ -12,9 +13,10 @@ function changeModal() {
   screen2.classList.toggle("hide");
 }
 
-function returnPageGame(e) {
+function resetGame(e) {
   xAttempts = 0;
   changeModal();
+  randomNumber = Math.round(Math.random() * 10);
 }
 
 function handleTryNumber(event) {
@@ -23,15 +25,14 @@ function handleTryNumber(event) {
   if (Number(inputNumber.value) == randomNumber) {
     changeModal();
 
-    document.querySelector(
-      ".screen2 h2"
-    ).innerText = `Acertou em ${xAttempts} ${
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} ${
       xAttempts >= 2 ? "tentativas" : "tentativa"
     }`;
   }
   inputNumber.focus();
 }
 
+// verifica se o input é valido
 function updateInput(event) {
   if (
     inputNumber.value >= 11 ||
@@ -43,6 +44,14 @@ function updateInput(event) {
   console.log(inputNumber.value.toString().length);
 }
 
+//eventos
+
 btnTry.addEventListener("click", handleTryNumber);
-btnPlayAgain.addEventListener("click", returnPageGame);
+btnPlayAgain.addEventListener("click", resetGame);
 inputNumber.addEventListener("input", updateInput);
+document.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    if (!screen2.classList.contains("hide")) resetGame(e);
+    else handleTryNumber(e);
+  }
+});
